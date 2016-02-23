769eb6b560bea834e3e5edd5cf06c6c4a6238156
chiricos <edwarddiaz92@gmail.com>
2016-02-23 08:28:47 -0500

    subiendo cruz

---
+++ wp-content/themes/sw_atom/footer.php
@@ -0,0 +1,68 @@
+<?php if (is_active_sidebar_YA('above-footer')){ ?>
+
+<div class="sidebar-above-footer theme-clearfix">
+       <div class="container theme-clearfix">	   
+		<?php dynamic_sidebar('above-footer'); ?>
+	    </div>
+</div>
+<?php } ?>
+<div class="condiciones">
+	<ul>		
+		<li><a href="http://localhost/atom/?page_id=4402"><img src="http://localhost/atom/wp-content/uploads/2016/02/icono-politicas.png">Políticas Cruz</a></li>
+		<li><a href="http://localhost/atom/?page_id=5006"><img src="http://localhost/atom/wp-content/uploads/2016/02/icono-cuidados.png">Cuidado de tus muebles</a></li>		
+		<li><a href="http://localhost/atom/?page_id=4379"><img src="http://localhost/atom/wp-content/uploads/2016/02/icono-envios.png">Envío</a></li>
+		<li><a href="http://localhost/atom/?page_id=635"><img src="http://localhost/atom/wp-content/uploads/2016/02/icono-quienessomos.png">Quiénes somos</a></li>
+		<li><a href="http://localhost/atom/?page_id=642"><img src="http://localhost/atom/wp-content/uploads/2016/02/icono-contacto.png">Contáctenos</a></li>
+	</ul>
+</div>
+<footer>
+	<div class="container">	
+		<div class="mark">			
+				<img src="http://localhost/atom/wp-content/uploads/2016/01/mediospago.png">									
+		</div>
+	</div>
+	<div class="copy">
+		<p>INDUSTRIAS METALICAS CRUZ (C) TODOS LOS DERECHOS RESERVADOS 2016 / <a href="">Términos y condiciones</a></p>
+	</div>	
+</footer>
+<!--<footer class="footer theme-clearfix" >
+	<div class="container theme-clearfix">
+		<div class="footer-top">
+			<div class="row">
+				<?php if (is_active_sidebar_YA('footer')){ ?>
+									
+						<?php dynamic_sidebar('footer'); ?>
+					
+				<?php } ?>
+			</div>
+		</div>
+	</div>
+	<div class="copyright theme-clearfix">
+		<div class="container clearfix">
+			<div class="col-lg-8 col-md-8 col-sm-8 pull-left clearfix">
+				<div class="copyright-text pull-left">
+					<p>&copy;<?php echo date('Y'); ?> <a href="http://www.smartaddons.com/wordpress/theme-showcase"><?php esc_html_e('Wordpress Theme ', 'yatheme'); ?></a><?php esc_html_e('SW Atom Store. All Rights Reserved. Designed by ','yatheme'); ?><a class="mysite" href="http://www.smartaddons.com/"><?php esc_html_e('Smartaddons.com','yatheme');?></a>.</p>
+				</div>
+			</div>
+			<?php if (is_active_sidebar_YA('footer-copyright')){ ?>
+				<div class="sidebar-copyright pull-right">
+						<div class="col-md-12 theme-clearfix">
+							<?php dynamic_sidebar('footer-copyright'); ?>
+						</div>
+				</div>
+			<?php } ?>
+		</div>
+	</div>
+</footer>-->
+<?php if (is_active_sidebar_YA('floating') ){ ?>
+<div class="floating theme-clearfix">
+	<?php dynamic_sidebar('floating');  ?>
+</div>
+</div>
+<?php } ?>
+<?php if(ya_options()->getCpanelValue('back_active') == '1') { ?>
+<a id="ya-totop" href="#" ></a>
+<?php }?>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/sw_atom/header.php
@@ -0,0 +1,133 @@
+<?php $ya_box_layout = ya_options()->getCpanelValue('layout'); ?>	
+<?php get_template_part('templates/head'); ?>
+<body <?php body_class(); ?>>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.5&appId=841499242639414";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="menu-top">
+	<div class="container">
+		<ul>
+			<li><a href="http://localhost/atom/?page_id=751">Proyectos</a></li>
+			<li><a href="http://localhost/atom/?page_id=3215">Puntos de experiencia</a></li>
+			<li><a href=" http://localhost/atom/?page_id=4277">Catálogos</a></li>			
+			<li><a href="http://localhost/atom/?page_id=635">Conócenos</a></li>
+			<li><a href="http://localhost/atom/?page_id=642">Contáctenos</a></li>
+			<li><a href="http://localhost/atom/?page_id=4175&preview=true">Garantía</a></li>
+			<li><a href="http://localhost/atom/?page_id=4379">Envío</a></li>
+			<li><a href="http://localhost/atom/?page_id=4402">Compra</a></li>
+		</ul> 
+	<div class="top-social">
+		<a href="https://www.facebook.com/Met%C3%A1licas-Cruz-212039442461429/?ref=bookmarks" target="_blank">
+			<span class="fa fa-facebook icon-social"></span>
+		</a>
+		<a href="https://fb.com" target="_blank">
+			<span class="fa  fa-twitter icon-social"></span>
+		</a>
+		<a href="https://plus.google.com/u/0/115231477183451786072/posts" target="_blank">
+			<span class="fa fa-google-plus icon-social"></span>
+		</a>
+		<a href="https://fb.com" target="_blank">
+			<span class="fa fa-linkedin icon-social"></span>
+		</a>
+		<a href="https://www.pinterest.com/metalicascruz/" target="_blank">
+			<span class="fa fa-pinterest icon-social"></span>
+		</a>
+	</div>
+	</div>
+	
+</div>
+<div class="body-wrapper theme-clearfix<?php echo ( $ya_box_layout == 'boxed' )? ' box-layout' : '';?> ">
+<?php 
+	$ya_colorset = ya_options()->getCpanelValue('scheme');
+	$ya_header_style = ya_options()->getCpanelValue('header_style');
+if ($ya_header_style == 'default'){
+?>
+
+<header id="header" role="banner" class="header">
+
+    <div class="header-msg">
+        <div class="container">
+        <?php if (is_active_sidebar_YA('top')) {?>
+            <div id="sidebar-top" class="sidebar-top">
+                <?php dynamic_sidebar('top'); ?>
+            </div>
+        <?php }?>
+        </div>
+    </div>
+	<div class="container">
+		<div class="top-header">			
+			<div class="ya-logo pull-left">
+				<a  href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<?php if(ya_options()->getCpanelValue('sitelogo')){ ?>
+							<img src="<?php echo esc_attr( ya_options()->getCpanelValue('sitelogo') ); ?>" alt="<?php bloginfo('name'); ?>"/>
+						<?php }else{
+							if ($ya_colorset){$logo = get_template_directory_uri().'/assets/img/logo-'.$ya_colorset.'.png';}
+							else $logo = get_template_directory_uri().'/assets/img/logo-default.png';
+						?>
+							<img src="<?php echo esc_attr( $logo ); ?>" alt="<?php bloginfo('name'); ?>"/>
+						<?php } ?>
+				</a>
+			</div>
+			<?php if (is_active_sidebar_YA('top-header')) {?>
+				<div id="sidebar-top-header" class="sidebar-top-header">
+						<?php dynamic_sidebar('top-header'); ?>
+				</div>
+			<?php }?>
+		</div>
+	</div>
+</header>
+
+<?php if ( has_nav_menu('primary_menu') ) {?>
+	<!-- Primary navbar -->
+<div id="main-menu" class="main-menu">
+	<nav id="primary-menu" class="primary-menu" role="navigation">
+		<div class="container">
+			<div class="mid-header clearfix">
+				<a href="#" class="phone-icon-menu"></a>
+				<div class="navbar-inner navbar-inverse">
+						<?php
+							$ya_menu_class = 'nav nav-pills';
+							if ( 'mega' == ya_options()->getCpanelValue('menu_type') ){
+								$ya_menu_class .= ' nav-mega';
+							} else $ya_menu_class .= ' nav-css';
+						?>
+						<?php wp_nav_menu(array('theme_location' => 'primary_menu', 'menu_class' => $ya_menu_class)); ?>
+				</div>
+				<?php if (is_active_sidebar_YA('top-menu')) {?>
+					<div id="sidebar-top-menu" class="sidebar-top-menu">
+							<?php dynamic_sidebar('top-menu'); ?>
+					</div>
+				<?php }?>
+			</div>
+		</div>
+	</nav>
+</div>
+
+	<!-- /Primary navbar -->
+
+
+<?php 
+	}
+} else {
+    echo '<div class="header-' . $ya_header_style . '">';
+    get_template_part('templates/header', $ya_header_style);
+    echo '</div>';
+}	
+?>
+
+<div id="main" class="theme-clearfix" role="document">
+<?php
+	if (!is_front_page() ) {
+		if (function_exists('ya_breadcrumb')){
+			ya_breadcrumb('<div class="breadcrumbs theme-clearfix"><div class="container">', '</div></div>');
+		} 
+	} 
+
+?>
